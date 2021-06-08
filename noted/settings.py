2a81a0f421d595d5ffd364b6c7fffb89994159ae
Mubarak Yahaya <Mubrikdev@gmail.com>
2021-06-08 09:50:49 +0100

    removed collectstatic command causing deployment script to fail, added a default static_root

--- noted/settings.py
+++ noted/settings.py
@@ -124,33 +124,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'noted.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
-else: 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'django_rest',
-            'USER': 'djangoadmin',
-            'PASSWORD': 'django@321',
-            'HOST': 'localhost',
-            'PORT': '5432',
-        }
-    }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
