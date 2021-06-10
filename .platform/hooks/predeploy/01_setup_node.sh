7f096f7c6890500be020f375e73675b24f506d1d
Mubarak Yahaya <Mubrikdev@gmail.com>
2021-06-10 19:25:01 +0100

    test sucess, updated script to run build

--- .platform/hooks/predeploy/01_setup_node.sh
+++ .platform/hooks/predeploy/01_setup_node.sh
@@ -4,6 +4,10 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 . ~/.nvm/nvm.sh
 
-nvm install node
+nvm install 15.0.1
 
 node -e "console.log('Running Node.js ' + process.version)"
+
+npm install && npm run build
+
+npm list --prod --depth=0 --json
