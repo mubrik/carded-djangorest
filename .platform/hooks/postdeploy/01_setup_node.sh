2e642fad84adc2c2939f39d46969f7a17f8b53c7
Mubarak Yahaya <Mubrikdev@gmail.com>
2021-06-11 19:11:08 +0100

    still testing

--- .platform/hooks/postdeploy/01_setup_node.sh
+++ .platform/hooks/postdeploy/01_setup_node.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 TASKS_DIR=/opt/elasticbeanstalk/tasks
-echo $NODE_LOG_PATH
-
-echo "${NODE_LOG_PATH//.log/*}" > "$TASKS_DIR/bundlelogs.d/01-app-log.conf"
-# include current app log file in tail logs
-echo $NODE_LOG_PATH > "$TASKS_DIR/taillogs.d/01-app-log.conf"
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
@@ -15,21 +10,27 @@ nvm install 15.0.1
 
 node -e "console.log('Running Node.js ' + process.version)"
 
-# install devdependencies
-npm install |& tee "$NODE_LOG_PATH"
+# create log
+touch nodelog.log
+chmod +wr nodelog.log
 
-pwd
+# install devdependencies
+npm install |& tee nodelog.log
+
 ls -a -l
 
 # permission
 chmod +x package.json
-chmod +wr frontend
+chmod +x node_modules/.bin/webpack
+chmod +wr -R /frontend
 
 ls -a -l
 
 
 # create static
-npm run build |& tee -a $NODE_LOG_PATH
+npm run build |& tee -a nodelog.log
 
 # print for logs
-npm list --depth=0 --json |& tee -a $NODE_LOG_PATH
+npm list --depth=0 --json |& tee -a nodelog.log
+nodelog.log > "$TASKS_DIR/bundlelogs.d/01-app-log.conf"
+nodelog.log > "$TASKS_DIR/taillogs.d/01-app-log.conf"
