45ebb987b63bde0604aa7f7de4bc2465540189e4
Mubarak Yahaya <Mubrikdev@gmail.com>
2021-06-11 17:18:22 +0100

    adding log file env-variable to log npm changes, still testing

--- .platform/hooks/postdeploy/01_setup_node.sh
+++ .platform/hooks/postdeploy/01_setup_node.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+TASKS_DIR=/opt/elasticbeanstalk/tasks
+
+echo "${NODE_LOG_PATH//.log/*}" > "$TASKS_DIR/bundlelogs.d/01-app-log.conf"
+# include current app log file in tail logs
+echo $NODE_LOG_PATH > "$TASKS_DIR/taillogs.d/01-app-log.conf"
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 . ~/.nvm/nvm.sh
@@ -9,12 +15,14 @@ nvm install 15.0.1
 node -e "console.log('Running Node.js ' + process.version)"
 
 # install devdependencies
-npm install --production=false
+npm install --production=false |& tee -a $NODE_LOG_PATH
 
 pwd
 
+ls -a -l
+
 # create static
-npm run build
+npm run build |& tee -a $NODE_LOG_PATH
 
 # print for logs
-npm list --depth=0 --json
+npm list --depth=0 --json |& tee -a $NODE_LOG_PATH
